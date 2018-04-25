795045fb8ceff0a1c2b71768cb40a1b1fa55d0d2
zsqio <1248411877@qq.com>
2018-04-25 18:06:31 +0800

    update

--- pages/book/index.js
+++ pages/book/index.js
@@ -12,6 +12,29 @@ Page({
         autoplay: true,
         interval: 5000,
         duration: 1000,
+        bookList: [
+            {
+                imgUrl:'../../images/banner/banner3.jpg',
+                iconUrl:'../../images/bookIcon/ding.jpg',
+                title:'丁丁历险记',
+                subTitle: '跟着丁丁去历险',
+                price: 89,
+            },
+            {
+                imgUrl:'../../images/banner/banner4.jpg',
+                iconUrl:'../../images/bookIcon/pig.jpg',
+                title:'小猪佩奇故事书系列',
+                subTitle: '二十册佩奇故事书画册',
+                price: 126,
+            },
+            {
+                imgUrl:'../../images/banner/banner2.jpg',
+                iconUrl:'../../images/bookIcon/time.jpg',
+                title:'诚品时光',
+                subTitle: '首次了解诚品时光，绝不只是一家书店',
+                price: 56,
+            },
+        ]
     },
     onLoad() {
         console.log('onLoad')
